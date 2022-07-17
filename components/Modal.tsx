66df7455353a4bf59b38e643a54a41f4c5c10a1a
simon <simon.renault.pro@gmail.com>
2022-07-17 11:37:57 +0100

    test

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -4,6 +4,7 @@ import css from "./Modal.module.scss";
 
 import { X } from "react-feather";
 import Button from "./Button";
+import { AnimatePresence, motion } from "framer-motion";
 
 interface IModalProps {
 	show: boolean;
@@ -44,6 +45,88 @@ const VARIANTS = [
 	},
 ];
 
+const mobileMenuVariant = {
+	initial: {
+		y: "100%",
+		transition: {
+			delay: 0.15,
+			duration: 0.33,
+			ease: [0.74, 0, 0.19, 1.02],
+		},
+	},
+	animate: {
+		y: "0%",
+		transition: {
+			delay: 0.05,
+			duration: 0.6,
+			ease: [0.74, 0, 0.19, 1.02],
+		},
+	},
+	exit: {
+		y: "100%",
+		transition: {
+			duration: 0.33,
+			ease: [0.74, 0, 0.19, 1.02],
+		},
+	},
+};
+
+const fadeInVariant = {
+	animate: {
+		opacity: 1,
+	},
+	initial: {
+		opacity: 0,
+		transition: {
+			delay: 0.1,
+		},
+	},
+	exit: {
+		transition: {
+			delay: 0.5,
+		},
+		opacity: 0,
+	},
+};
+
+const fadeInContentVariant = {
+	animate: {
+		opacity: 1,
+		transition: {
+			delay: 0.7,
+			ease: "easeIn",
+		},
+	},
+	initial: {
+		opacity: 0,
+		transition: {
+			delay: 0.7,
+			ease: "easeIn",
+		},
+	},
+	exit: {
+		opacity: 1,
+		transition: {
+			ease: "easeIn",
+		},
+	},
+};
+
+const fadeIn = {
+	animate: {
+		opacity: 1,
+		transition: {
+			ease: "easeIn",
+		},
+	},
+	initial: {
+		opacity: 0,
+		transition: {
+			ease: "easeIn",
+		},
+	},
+};
+
 const SelectableCard = (props: ISelectableCardProps) => {
 	const { title, price, dimensions, isSelected, onClick } = props;
 	return (
@@ -75,58 +158,110 @@ const Modal = (props: IModalProps) => {
 		onClose();
 	};
 
-	const modalContent = show ? (
-		<div className={css.overlay}>
-			<div className={css.modal}>
-				<div className={css.preview}>
-					<div className={css.preview_image}></div>
-					<div className={css.thumbnails}>
-						<div className={css.thumbnail}></div>
-						<div className={css.thumbnail}></div>
-						<div className={css.thumbnail}></div>
-					</div>
-				</div>
-				<div className={css.config}>
-					<header className={css.header}>
-						<h3 className={css.modal_title}>About prints</h3>
-						<div
-							className={css.close_bottom}
-							onClick={handleCloseClick}
+	const modalContent = (
+		<AnimatePresence>
+			{show && (
+				<motion.div
+					className={css.overlay}
+					initial="initial"
+					animate="animate"
+					exit="exit"
+					variants={fadeInVariant}
+				>
+					<motion.div
+						className={css.modal}
+						initial="initial"
+						animate="animate"
+						exit="exit"
+						variants={mobileMenuVariant}
+					>
+						<motion.div
+							initial="initial"
+							animate="animate"
+							exit="exit"
+							variants={fadeInContentVariant}
+							className={css.preview}
 						>
-							<X />
-						</div>
-					</header>
-					<main className={css.main}>
-						<p>
-							All prints are made on high quality 300g paper and
-							are archival quality. They come hanpacked by myself
-							with a few surprises. c
-						</p>
-						<div className={css.divider}></div>
-						<h3 className={css.title}>Pick a Size</h3>
-						<div className={css.size_picker}>
-							{VARIANTS.map((card, index) => {
-								return (
-									<SelectableCard
-										title={card.title}
-										dimensions={card.dimensions}
-										price={card.price}
-										isSelected={selectedCard == index}
-										onClick={() => setSelectedCard(index)}
-										key={`variant-${index}`}
-									/>
-								);
-							})}
-						</div>
-						<p>{VARIANTS[selectedCard].message}</p>
-					</main>
-					<footer className={css.footer}>
-						<Button fill>Add to cart — 50€</Button>
-					</footer>
-				</div>
-			</div>
-		</div>
-	) : null;
+							<div className={css.preview_image}></div>
+							<div className={css.thumbnails}>
+								<div className={css.thumbnail}></div>
+								<div className={css.thumbnail}></div>
+								<div className={css.thumbnail}></div>
+							</div>
+						</motion.div>
+						<motion.div
+							initial="initial"
+							animate="animate"
+							exit="exit"
+							variants={fadeInContentVariant}
+							className={css.config}
+						>
+							<header className={css.header}>
+								<h3 className={css.modal_title}>
+									About prints
+								</h3>
+								<div
+									className={css.close_bottom}
+									onClick={handleCloseClick}
+								>
+									<X />
+								</div>
+							</header>
+							<main className={css.main}>
+								<p>
+									All prints are made on high quality 300g
+									paper and are archival quality. They come
+									hanpacked by myself with a few surprises.
+								</p>
+								<div className={css.divider}></div>
+								<h3 className={css.title}>Pick a Size :</h3>
+								<div className={css.size_picker}>
+									{VARIANTS.map((card, index) => {
+										return (
+											<SelectableCard
+												title={card.title}
+												dimensions={card.dimensions}
+												price={card.price}
+												isSelected={
+													selectedCard == index
+												}
+												onClick={() =>
+													setSelectedCard(index)
+												}
+												key={`variant-${index}`}
+											/>
+										);
+									})}
+								</div>
+								<AnimatePresence exitBeforeEnter>
+									{VARIANTS.map((card, index) => {
+										return (
+											index == selectedCard && (
+												<motion.p
+													initial="initial"
+													animate="animate"
+													exit="initial"
+													variants={fadeIn}
+													key={`msg-${index}`}
+												>
+													{card.message}
+												</motion.p>
+											)
+										);
+									})}
+								</AnimatePresence>
+							</main>
+							<footer className={css.footer}>
+								<Button fill>
+									Add to cart — {VARIANTS[selectedCard].price}
+								</Button>
+							</footer>
+						</motion.div>
+					</motion.div>
+				</motion.div>
+			)}
+		</AnimatePresence>
+	);
 
 	if (isBrowser) {
 		const el = document.getElementById("modal-root");
