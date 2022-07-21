022cf04aa3cc26ef2d41490f3239e99b84350827
simon <simon.renault.pro@gmail.com>
2022-07-21 18:24:11 +0100

    test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,18 +8,18 @@ import { ContextProvider } from "@lib/shopContext";
 import { AnimatePresence } from "framer-motion";
 import Router from "next/router";
 
+const tempFix = () => {
+	const allStyleElems = document.querySelectorAll('style[media="x"]');
+	allStyleElems.forEach((elem) => {
+		elem.removeAttribute("media");
+	});
+};
 const routeChange = () => {
 	// Temporary fix to avoid flash of unstyled content
 	// during route transitions. Keep an eye on this
 	// issue and remove this code when resolved:
 	// https://github.com/vercel/next.js/issues/17464
 
-	const tempFix = () => {
-		const allStyleElems = document.querySelectorAll('style[media="x"]');
-		allStyleElems.forEach((elem) => {
-			elem.removeAttribute("media");
-		});
-	};
 	tempFix();
 };
 
@@ -27,6 +27,7 @@ Router.events.on("routeChangeComplete", routeChange);
 Router.events.on("routeChangeStart", routeChange);
 
 function MyApp({ Component, pageProps, router }: AppProps): JSX.Element {
+	tempFix();
 	return (
 		<ContextProvider>
 			<Head>
