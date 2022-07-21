5ca0295166d7e689d9bf2f37544f93df82c21edc
simon <simon.renault.pro@gmail.com>
2022-07-21 18:11:41 +0100

    test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,8 +6,10 @@ import css from "./_app.module.scss";
 import Head from "next/head";
 import { ContextProvider } from "@lib/shopContext";
 import { AnimatePresence } from "framer-motion";
+import { Router } from "next/router";
 
 function MyApp({ Component, pageProps, router }: AppProps): JSX.Element {
+	fixTimeoutTransition(1000);
 	return (
 		<ContextProvider>
 			<Head>
@@ -21,6 +23,7 @@ function MyApp({ Component, pageProps, router }: AppProps): JSX.Element {
 			<div className={css.scroll_container}>
 				<AnimatePresence
 					exitBeforeEnter
+					initial={false}
 					onExitComplete={() => window.scrollTo(0, 0)}
 				>
 					<Component {...pageProps} key={router.route} />
@@ -33,3 +36,41 @@ function MyApp({ Component, pageProps, router }: AppProps): JSX.Element {
 }
 
 export default MyApp;
+
+export const fixTimeoutTransition = (timeout: number): void => {
+	Router.events.on("beforeHistoryChange", () => {
+		// Create a clone of every <style> and <link> that currently affects the page. It doesn't matter
+		// if Next.js is going to remove them or not since we are going to remove the copies ourselves
+		// later on when the transition finishes.
+		const nodes = document.querySelectorAll(
+			"link[rel=stylesheet], style:not([media=x])"
+		);
+		const copies = [...nodes].map(
+			(el) => el.cloneNode(true) as HTMLElement
+		);
+
+		for (let copy of copies) {
+			// Remove Next.js' data attributes so the copies are not removed from the DOM in the route
+			// change process.
+			copy.removeAttribute("data-n-p");
+			copy.removeAttribute("data-n-href");
+
+			// Add duplicated nodes to the DOM.
+			document.head.appendChild(copy);
+		}
+
+		const handler = () => {
+			// Emulate a `.once` method using `.on` and `.off`
+			Router.events.off("routeChangeComplete", handler);
+
+			window.setTimeout(() => {
+				for (let copy of copies) {
+					// Remove previous page's styles after the transition has finalized.
+					document.head.removeChild(copy);
+				}
+			}, timeout);
+		};
+
+		Router.events.on("routeChangeComplete", handler);
+	});
+};
